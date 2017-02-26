aafaab1bcd2239784153c93bcdc885fd4ee91109
buriosca <buriosca@outlook.cz>
2017-02-26 18:30:41 +0100

    201702261830
    Signed-off-by: buriosca <buriosca@outlook.cz>

---
+++ src/main/java/cz/burios/data/StartUp.java
@@ -0,0 +1,35 @@
+package cz.burios.data;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/StartUp")
+public class StartUp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public StartUp() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
